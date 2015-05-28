9b8fa6c683781d5b28cdae3ebcf77b04db0d8be8
Stefan Stevsic <stewdza@gmail.com>
2015-05-28 10:42:33 +0200

    New ControlSetpoint message added. Changes made accordingly to this

--- mavros/src/plugins/setpoint_control.cpp
+++ mavros/src/plugins/setpoint_control.cpp
@@ -17,15 +17,16 @@
 #include <mavros/mavros_plugin.h>
 #include <mavros/setpoint_mixin.h>
 #include <pluginlib/class_list_macros.h>
+#include <mavros/ControlSetpoint.h>
 
 #include <geometry_msgs/Pose.h>
 
 
 namespace mavplugin {
 /**
- * @brief Setpoint position plugin
+ * @brief Setpoint control plugin
  *
- * Send setpoint positions to FCU controller.
+ * Send setpoint to FCU for specified controller.
  */
 class SetpointControlPlugin : public MavRosPlugin {
 public:
@@ -39,21 +40,26 @@ public:
 		uas = &uas_;
 		sp_nh.param("mav_control_target", mav_control_target, 100);
 
-		control_sub = sp_nh.subscribe("setpoint", 10, &SetpointControlPlugin:setpoint_cb, this);
+		control_sub = sp_nh.subscribe("setpoint", 10, &SetpointControlPlugin::setpoint_cb, this);
 	}
+
+        const message_map get_rx_handlers() {
+		return { /* Rx disabled */ };
+	}
+
 
 private:
 	ros::NodeHandle sp_nh;
 	UAS *uas;
 
-	int mav_control_target;// TODO: change type to unit_8
+	int mav_control_target;
 
 	ros::Subscriber control_sub;
 
 	void send_setpoint_control_target_ned(float x, float y, float z, float yaw)
 	{
 		mavlink_message_t msg;
-		mavlink_msg_set_attitude_target_pack_chan(UAS_PACK_CHAN(uas), &msg,
+		mavlink_msg_set_control_target_local_ned_pack_chan(UAS_PACK_CHAN(uas), &msg,
 				x, y, z, yaw,
 				UAS_PACK_TGT(uas),
 				mav_control_target,
@@ -61,13 +67,13 @@ private:
 		UAS_FCU(uas)->send_message(&msg);
 	}
 
-	void setpoint_cb(const geometry_msgs::Pose &msg)
+	void setpoint_cb(const mavros::ControlSetpoint &msg)
 	{
 		float x, y, z, yaw;
-		x = msg.position.x;
-		y = msg.position.y;
-		z = msg.position.z;
-		// TODO: add yaw
+		x = msg.x;
+		y = msg.y;
+		z = msg.z;
+		yaw = msg.yaw;
 
 		send_setpoint_control_target_ned(x,y,z,yaw);
 	}
